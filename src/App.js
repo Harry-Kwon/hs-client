124819b58b85157c3bf68e2cd980728a47e8140e
Harry Kwon <harrykwon20@gmail.com>
2019-07-18 12:07:18 +0000

    Moved card search logic up to App class and implemented card rendering based on search info from api request. Also implemented reactive styling using flexbox and min-width, max-height

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: []
+    };
+  }
+
+  handleChange = (event)=>{
+    console.log(event.target.value)
+    fetch('https://api.harrykwon.dev/card?name=' + event.target.value)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({...this.state, cards: result.cards});
+      },
+      (error) =>  {
+        console.log(error);
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Searchbox name='name' onChange={this.handleChange}/>
+
+        <Cardlist cards={this.state.cards}/>
+      </div>
+    );
+  }
+}
+
+//TODO refactor - move logic into App and pass new props to Cardlist (onChangeHandler)
+// Searchbox will take onChangeHandler as a prop
+function Searchbox(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form>
+      <p>Name</p>
+      <input
+        type='text'
+        name={props.name}
+        className='card-search-input'
+        onChange={props.onChange}></input>
+    </form>
+  )
+}
+
+function Cardlist(props) {
+  const cardData = props.cards;
+  console.log(cardData);
+
+  const cards = cardData.map((c) => {
+    return(
+      <Card
+        key={c.id}
+        name={c.name}
+        image={c.image}
+      />
+    )
+  });
+
+  return (
+    <div id='card-container'>
+      <ul>
+        {cards}
+      </ul>
     </div>
-  );
+  )
+}
+
+function Card(props) {
+  return (
+    <a href='/'>
+      <img
+        className='Card'
+        src={props.image}
+        alt={props.name}
+      />
+    </a>
+  )
 }
 
 export default App;
